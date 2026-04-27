8ec36171bb24fba58b1617b52a726e20441894e9
Yuval Meron <153757907+YuvalMeron1997@users.noreply.github.com>
2026-04-27 14:51:35 +0300

    Update evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,1 +1,27 @@
+import numpy as np
+from sklearn.metrics import calinski_harabasz_score, adjusted_rand_score
+from .kmeans import assign_to_cluster
 
+
+def compute_calinski(rdd, centroids):
+    """
+    Compute Calinski-Harabasz score.
+    """
+
+    assigned = rdd.map(lambda x: assign_to_cluster(x, centroids)).collect()
+
+    labels = [x[0] for x in assigned]
+    data = np.array([x[1] for x in assigned])
+
+    return calinski_harabasz_score(data, labels)
+
+
+def compute_ari(class_labels, rdd, centroids):
+    """
+    Compute Adjusted Rand Index (ARI).
+    """
+
+    assigned = rdd.map(lambda x: assign_to_cluster(x, centroids)).collect()
+    predicted = [x[0] for x in assigned]
+
+    return adjusted_rand_score(class_labels, predicted)
